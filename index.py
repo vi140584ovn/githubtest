c827c538d54e45f02609a1853e75159c1805ee0b
vi140584ovn <vi140584ovn@gmail.com>
2020-06-26 14:41:53 +0300

    Add existing file

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', color='red')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
